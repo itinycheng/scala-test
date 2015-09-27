bbd8738d0aeb3a07cca283417b766e821c11b2de
itinycheng <tiny,wang@foxmail.com>
2015-09-27 22:36:34 +0800

    actor test

---
+++ src/com/test/scala/actor/AkkaActorTest.scala
@@ -0,0 +1,23 @@
+package com.test.scala.actor
+
+import akka.actor.Actor
+import akka.actor.ActorSystem
+import akka.actor.Props
+
+object AkkaActorTest extends App{
+  val actsys = ActorSystem();
+  
+  class Hi extends Actor{
+    def receive = {
+      case msg:String => println("hello " + msg)
+    }
+  }
+  
+  def show(m : String) = {
+    val act = actsys.actorOf(Props[Hi])
+    act ! m
+    act ! m + ", nice to see you"
+  }
+  val names = Array[String]("kafka","hadoop","storm")
+  for(name <- names) show(name)
+}
